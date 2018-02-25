4cdeec190e9d32c2c26970530856a69e4541344b
Jonas <jonas.mueller.contact@gmail.com>
2018-02-26 00:03:29 +0100

    Changed npm scripts for dev and production and made production build available

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ Uses react, electron, ytdl-core & ffmpeg plus materialize-css for styling.
 This is my first React & Electron project and is quite hacky,
 please excuse all the structuring mistakes and lack of best practices.
 
-Use `npm run dev` to launch React and Electron together
+Use `npm run electron-dev` to launch React and Electron together.
 
-![Alt text](/public/logo.png?raw=true)
+Use `npm run dist` to build a distributable version.
+
+
+![Alt text](/public/icon.png?raw=true)
--- public/electron.js
+++ public/electron.js
@@ -3,20 +3,13 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
-const url = require('url');
+const isDev = require('electron-is-dev');
 
 let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({width: 1200, height: 800});
-
-    const startUrl = process.env.ELECTRON_START_URL || url.format({
-            pathname: path.join(__dirname, '/../build/index.html'),
-            protocol: 'file:',
-            slashes: true
-        });
-    mainWindow.loadURL(startUrl);
-
+    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
     mainWindow.on('closed', function () {
         mainWindow = null
     })
@@ -27,5 +20,11 @@ app.on('ready', createWindow);
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit()
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
     }
 });
